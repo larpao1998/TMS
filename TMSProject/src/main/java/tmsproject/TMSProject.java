928309aaf904433b21fd9984a723671cc58676cc
Alexandre <Alexandre@AlexPC.lan>
2022-04-28 02:35:22 -0400

    1. Worked on "addPage". 2. Established connection with MySQL workbench. 3. pom.xml dependency added for sql connection.

--- TMSProject/src/main/java/tmsproject/TMSProject.java
+++ TMSProject/src/main/java/tmsproject/TMSProject.java
@@ -5,15 +5,34 @@
 
 package tmsproject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.*;
+
 /**
  *
  * @author Alexandre
  */
-public class TMSProject {
+public class TMSProject extends javax.swing.JFrame{
 
     public static void main(String[] args) {
-        System.out.println("testing repo");
-        System.out.println("testing repo 2");
-        System.out.println("testing repo 3");
-    }
+        String url ="jdbc:mysql://localhost:3306/tms database";
+        String username = "TMS_User";
+        String password = "TMS2022";
+        
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url, username, password);
+            System.out.println("Successfull" + url);
+            
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        } catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+        
+    }//END main() method
 }
